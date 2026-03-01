bcbb9edbe953c4a4f2dd8e276d1c93200baf3a2a
Mcode4 <lamllc20@gmail.com>
2026-02-28 19:49:55 -0500

    routes updated

--- backend/app/routes/__init__.py
+++ backend/app/routes/__init__.py
@@ -2,12 +2,16 @@ from fastapi import APIRouter, Response
 from app.routes.auth import router as auth_router
 from app.routes.property import router as property_router
 from app.routes.images import router as image_router
+from app.routes.users import router as users_router
+from app.routes.floors import router as floors_router
 
 router = APIRouter(prefix="/api", tags=["API"])
 
 router.include_router(auth_router)
 router.include_router(property_router)
 router.include_router(image_router)
+router.include_router(users_router)
+router.include_router(floors_router)
 
 @router.get("/")
 def health_check():
