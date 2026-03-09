c9abded87e964c78d4030f667072ee395072b667
Mcode4 <lamllc20@gmail.com>
2026-03-09 14:08:42 -0400

    deploy

--- start.sh
+++ start.sh
@@ -6,5 +6,5 @@ python backend/scripts/migrate_db_to_psql.py
 echo "Starting FastAPI..."
 uvicorn backend.main:app --host 0.0.0.0 --port 8000 &
 
-exho "Starting Nginx..."
+echo "Starting Nginx..."
 nginx -g "daemon off;"
