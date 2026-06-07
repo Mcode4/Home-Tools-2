5beae71aaad5158745c5a7026f74b00a7f15d02d
Kilo <kilo@kilo.ai>
2026-06-07 05:21:10 -0400

    fix(6.2): Replace TransformControls with manual RGB axis gizmo for selected objects

--- frontend/src/components/RenderPageComponents/RenderComponent/FurnitureObject.jsx
+++ frontend/src/components/RenderPageComponents/RenderComponent/FurnitureObject.jsx
@@ -20,63 +20,6 @@ function BoxFurniture({ object }) {
     );
 }
 
-function ObjectWithTransform({ object, isSelected, onClick }) {
-    const groupRef = useRef();
-    const transformRef = useRef();
-    const rotation = (object.rotation || 0) * (Math.PI / 180);
-    const h = object.height3d || 20;
-
-    useEffect(() => {
-        if (transformRef.current && groupRef.current) {
-            transformRef.current.attach(groupRef.current);
-        }
-    }, []);
-
-    return (
-        <TransformControls
-            ref={transformRef}
-            mode="translate"
-            size={0.7}
-            onDragEnd={() => {
-                if (groupRef.current) {
-                    const pos = groupRef.current.position;
-                    onClick?.({ ...object, x: pos.x, y: pos.z });
-                }
-            }}
-        >
-            <group
-                ref={groupRef}
-                position={[object.x, h / 2, object.y]}
-                rotation={[0, rotation, 0]}
-                onClick={(e) => {
-                    e.stopPropagation();
-                    onClick?.(object.id);
-                }}
-            >
-                {object.modelUrl && object.modelUrl.startsWith("data:") ? (
-                    <Suspense fallback={null}>
-                        <GLBModel url={object.modelUrl} />
-                    </Suspense>
-                ) : (
-                    <BoxFurniture object={object} />
-                )}
-                {isSelected && (
-                    <mesh>
-                        <boxGeometry args={[(object.width || 40) + 2, h + 2, (object.height || 40) + 2]} />
-                        <meshBasicMaterial color="#22c55e" wireframe={true} />
-                    </mesh>
-                )}
-                {isSelected && (
-                    <mesh position={[0, h / 2 + 5, 0]}>
-                        <sphereGeometry args={[3, 8, 8]} />
-                        <meshBasicMaterial color="#22c55e" />
-                    </mesh>
-                )}
-            </group>
-        </TransformControls>
-    );
-}
-
 export default function FurnitureObject({ object, isSelected, onClick, useTransformControls = false }) {
     if (!object) return null;
 
@@ -111,12 +54,39 @@ export default function FurnitureObject({ object, isSelected, onClick, useTransf
                     <meshBasicMaterial color="#22c55e" />
                 </mesh>
             )}
+            {isSelected && (
+                <group position={[0, h / 2, 0]}>
+                    {/* X axis arrow (red) */}
+                    <mesh position={[15, 0, 0]} rotation={[0, 0, -Math.PI / 2]}>
+                        <cylinderGeometry args={[0.5, 0.5, 20, 8]} />
+                        <meshBasicMaterial color="#ff4444" />
+                    </mesh>
+                    <mesh position={[26, 0, 0]}>
+                        <coneGeometry args={[2, 4, 8]} />
+                        <meshBasicMaterial color="#ff4444" />
+                    </mesh>
+                    {/* Y axis arrow (green) */}
+                    <mesh position={[0, 15, 0]}>
+                        <cylinderGeometry args={[0.5, 0.5, 20, 8]} />
+                        <meshBasicMaterial color="#44ff44" />
+                    </mesh>
+                    <mesh position={[0, 26, 0]}>
+                        <coneGeometry args={[2, 4, 8]} />
+                        <meshBasicMaterial color="#44ff44" />
+                    </mesh>
+                    {/* Z axis arrow (blue) */}
+                    <mesh position={[0, 0, 15]} rotation={[Math.PI / 2, 0, 0]}>
+                        <cylinderGeometry args={[0.5, 0.5, 20, 8]} />
+                        <meshBasicMaterial color="#4444ff" />
+                    </mesh>
+                    <mesh position={[0, 0, 26]} rotation={[Math.PI / 2, 0, 0]}>
+                        <coneGeometry args={[2, 4, 8]} />
+                        <meshBasicMaterial color="#4444ff" />
+                    </mesh>
+                </group>
+            )}
         </group>
     );
-
-    if (useTransformControls && isSelected) {
-        return <ObjectWithTransform object={object} isSelected={isSelected} onClick={onClick} />;
-    }
 
     return content;
 }
